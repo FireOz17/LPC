ec29a8e48f93bfc1a3f7be3d96948bd1fae7f181
FireOz17 <71986444+FireOz17@users.noreply.github.com>
2020-11-27 04:58:30 -0600

    Update Practica-9-OZIEL.py

--- Practica-9-OZIEL.py
+++ Practica-9-OZIEL.py
@@ -15,8 +15,8 @@ msg = MIMEMultipart()
 message = "Hola soy Oziel, de clase de los Sabados a las 12pm."
 
  
-msg['From'] = 'oziel.mendozacr@uanl.edu.mx'
-receipents = ["ozielmdza@outlook.com"]
+msg['From'] = '------------------------'
+receipents = ["---------------------"]
 msg['To'] = ", ".join(receipents)
 msg['Subject'] = 'Salu2!'
 
@@ -25,7 +25,7 @@ msg.attach(MIMEText(message, 'plain'))
 server = smtplib.SMTP('smtp.office365.com:587')
 server.starttls()
 
-server.login('oziel.mendozacr@uanl.edu.mx','Fisicomatematico12')
+server.login('oziel.mendozacr@uanl.edu.mx','--------------------')
 
 server.sendmail(msg['From'], receipents, msg.as_string())
 server.quit()
