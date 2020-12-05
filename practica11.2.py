c1c25296093a304c87c5e06d7365c444ca309d47
FireOz17 <71986444+FireOz17@users.noreply.github.com>
2020-12-04 18:34:41 -0600

    Add files via upload

---
+++ practica11.2.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+from tkinter import *
+
+app = tk.Tk()
+app.title("PRACTICA_11")
+etiqueta = Label(app, text="Codigo: FireOz en el Fortnite!!!")
+boton = Button(app, text="Claro")
+
+etiqueta.pack()
+boton.pack()
+app.mainloop()
+
+
---
+++ practica11.py
@@ -0,0 +1,5 @@
+print("Codigo FireOz_ en la tienda del fornais,ah no practica 11 xd")
+input("...")
+
+
+
